04f08d20cfc49e1047c178a4ac9ff1f5cb73467c
Konstantin Cherkashin <tcherkashin94@gmail.com>
2024-11-03 00:06:05 -0600

    default sf symbol sorting

--- Sources/SFSymbolsPicker/SymbolLoader.swift
+++ Sources/SFSymbolsPicker/SymbolLoader.swift
@@ -27,16 +27,24 @@ public class SymbolLoader {
 
     // Loads all symbols from the plist file
     private func getAllSymbols() -> [Symbol] {
-        if let bundle = Bundle(identifier: "com.apple.CoreGlyphs"),
-           let resourcePath = bundle.path(forResource: "name_availability", ofType: "plist"),
-           let plist = NSDictionary(contentsOfFile: resourcePath),
-           let plistSymbols = plist["symbols"] as? [String: String],
-           let searchIndexPlistPath = bundle.path(forResource: "symbol_search", ofType: "plist"),
-           let parsedSearchIndex = NSDictionary(contentsOfFile: searchIndexPlistPath) as? [String: [String]]
+        if
+            let bundle = Bundle(identifier: "com.apple.CoreGlyphs"),
+            let resourcePath = bundle.path(forResource: "name_availability", ofType: "plist"),
+            let plist = NSDictionary(contentsOfFile: resourcePath),
+            let plistSymbols = plist["symbols"] as? [String: String],
+            let searchIndexPlistPath = bundle.path(forResource: "symbol_search", ofType: "plist"),
+            let parsedSearchIndex = NSDictionary(contentsOfFile: searchIndexPlistPath) as? [String: [String]],
+            let symbolOrderPlistURL = bundle.url(forResource: "symbol_order", withExtension: "plist"),
+            let parsedSymbolOrder = NSArray(contentsOf: symbolOrderPlistURL) as? [String]
         {
-            return Array(plistSymbols.keys).sorted(by: {
-                $1 > $0
-            }).map {
+
+            // Create a lookup dictionary for order indices
+            let orderLookup = Dictionary(uniqueKeysWithValues: parsedSymbolOrder.enumerated().map { ($1, $0) })
+
+            // Sort symbols based on the precomputed order indices
+            return Array(plistSymbols.keys).sorted {
+                (orderLookup[$0] ?? Int.max) < (orderLookup[$1] ?? Int.max)
+            }.map {
                 Symbol(systemIconName: $0, aliases: parsedSearchIndex[$0] ?? [])
             }
         }
